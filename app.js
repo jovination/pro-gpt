21982f14f3401b3e36c49d3d70ae899871239afb
jovination <jovination791@gmail.com>
2024-01-26 04:08:47 +0300

    few updates on js for accordion

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const accordionItem = document.querySelectorAll(".accordion_item");
 
+function closeAllItems(exceptIndex) {
+    accordionItem.forEach((item, index) => {
+        if (index !== exceptIndex) {
+            let descriptions = item.querySelector(".descriptions");
+            item.classList.remove("open");
+            descriptions.style.height = "0px";
+            item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+    });
+}
+
 accordionItem.forEach((item, index) => {
     let accordionHeader = item.querySelectorAll(".accordion_header");
     
     accordionHeader.forEach(header => {
         header.addEventListener("click", () => {
+            closeAllItems(index);
             item.classList.toggle("open");
             let descriptions = item.querySelector(".descriptions");
             if(item.classList.contains("open")){
                 descriptions.style.height = `${descriptions.scrollHeight}px`;
+                item.querySelector("i").classList.replace("fa-plus", "fa-minus");
             }
             else {
                 descriptions.style.height = "0px";
+                item.querySelector("i").classList.replace("fa-minus", "fa-plus");
             }
-           console.log(descriptions);
+            console.log(descriptions);
         });
     });
 });
